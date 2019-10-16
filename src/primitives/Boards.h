189b1ee02722e0df31f30702bac2563be641b5f3
Mauricio Jost <mauricio.jost@gmail.com>
2019-10-16 08:23:13 +0200

    Boards provides updateFirmware from project/platform/version

--- src/primitives/Boards.h
+++ src/primitives/Boards.h
@@ -9,6 +9,16 @@
 
 enum WifiNetwork { WifiNoNetwork = 0, WifiMainNetwork, WifiBackupNetwork };
 
+#ifndef MAIN4INOSERVER_API_HOST_BASE 
+#define MAIN4INOSERVER_API_HOST_BASE "http://martinenhome.com/main4ino/prd"
+#endif // MAIN4INOSERVER_API_HOST_BASE 
+
+#ifndef FIRMWARE_UPDATE_URL
+// base + project + platform + version
+#define FIRMWARE_UPDATE_URL MAIN4INOSERVER_API_HOST_BASE "/firmwares/%s/firmware-%s.%s.bin"
+#endif // FIRMWARE_UPDATE_URL
+
+
 WifiNetwork detectWifi(const char *ssid, const char *ssidb);
 bool initializeWifi(const char *ssid, const char *pass, const char *ssidb, const char *passb, bool skipIfConnected, int retries);
 void stopWifi();
@@ -16,10 +26,16 @@ int httpGet(const char *url, ParamStream *response, Table *headers);
 int httpPost(const char *url, const char *body, ParamStream *response, Table *headers);
 bool readFile(const char *fname, Buffer *content);
 bool writeFile(const char *fname, const char *content);
-void updateFirmwareVersion(const char *url, const char *projVersion);
+void updateFirmware(const char *url);
 bool lightSleepInterruptable(time_t cycleBegin, time_t periodSecs, int miniPeriodMsec, bool (*interrupt)(), void (*heartbeat)());
 bool lightSleepNotInterruptable(time_t cycleBegin, time_t periodSecs, void (*heartbeat)());
 void deepSleepNotInterruptable(time_t cycleBegin, time_t periodSecs);
 void deepSleepNotInterruptableSecs(time_t cycleBegin, time_t periodSecs);
+
+void updateFirmware(const char *project, const char* platform, const char *projVersion) {
+  Buffer aux(128);
+  aux.fill(FIRMWARE_UPDATE_URL, project, platform, version);
+  updateFirmware(aux.getBuffer());
+}
 
 #endif // BOARDS_INC
