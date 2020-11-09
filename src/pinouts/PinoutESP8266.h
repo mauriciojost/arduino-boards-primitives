bf24f043ff14df6c8b96f80d0b20b28d982b286c
Mauricio Jost <mauriciojost@gmail.com>
2020-11-10 00:24:45 +0100

    Update info on pinouts for esp8266

--- src/pinouts/PinoutESP8266.h
+++ src/pinouts/PinoutESP8266.h
@@ -1,38 +1,47 @@
 #ifdef ESP8266 // NODEMCU based on ESP8266
 
-#define GPIO0_PIN 0
-#define GPIO1_PIN 1
-#define GPIO2_PIN 2
-#define GPIO3_PIN 3
-#define GPIO4_PIN 4
-#define GPIO5_PIN 5
-#define GPIO6_PIN 6
-#define GPIO7_PIN 7
-#define GPIO8_PIN 8
-#define GPIO9_PIN 9
-#define GPIO10_PIN 10
-#define GPIO11_PIN 11
-#define GPIO12_PIN 12
-#define GPIO13_PIN 13
-#define GPIO14_PIN 14
-#define GPIO15_PIN 15
-#define GPIO16_PIN 16
+// CONSIDERATIONS FOR THE CIRCUIT 
+
+// From 
+// - https://tttapa.github.io/ESP8266/Chap04%20-%20Microcontroller.html
+// - https://i.pinimg.com/originals/f7/81/ef/f781efedf44a3b9673f1e88d2d8253c8.png
+
+// GPIO15  GPIO0   GPIO2
+//     0       0       1   UART BOOTLOADER
+//     0       1       1   RUN MODE (in every boot, heads up or may unflash/corrupt your firmware)
+//
+// Heads up when having pull-up/pull-down resistors in such pins!
+//
+// ....
+// [testing if needed all below] use resistors of 500Kohm for such
+// Ensure:
+//   VDD-----220uF---GND
+//   EN------500kohm---VDD
+//   RST--+--500kohm---VDD
+//        \--470pf-----GND
+//   GPIO0---500kohm---VDD
+//   GPIO2---500kohm---VDD
+//   GPIO15--500kohm---VDD
+//   GPIO16------------RST (only in case of deep sleep) 
+
+
+#define GPIO0_PIN 0 // pull-up?, participates in boot mode
+#define GPIO1_PIN 1 // pull-up?
+#define GPIO2_PIN 2 // pull-up?, participates in boot mode
+#define GPIO3_PIN 3 // pull-up?
+#define GPIO4_PIN 4 // pull-up
+#define GPIO5_PIN 5 // pull-up
+//#define GPIO6_PIN 6 // pull-up, can't be used as connected to flash
+//#define GPIO7_PIN 7 // pull-up, can't be used as connected to flash
+//#define GPIO8_PIN 8 // pull-up, can't be used as connected to flash
+//#define GPIO9_PIN 9 // pull-up, can't be used as connected to flash
+//#define GPIO10_PIN 10 // pull-up, can't be used as connected to flash
+//#define GPIO11_PIN 11 // pull-up, can't be used as connected to flash
+#define GPIO12_PIN 12 // pull-up
+#define GPIO13_PIN 13 // pull-up
+#define GPIO14_PIN 14 // pull-up
+#define GPIO15_PIN 15 // pull-up, participates in boot mode
+#define GPIO16_PIN 16 // pull-down
 #define A0_PIN 17
-
-// NODEMCU based on ESP8266 (human names)
-
-#define PIN_D0 GPIO16_PIN // working as OUTPUT, breaks deep sleep mode, NODE-MCU built-in !LED
-#define PIN_D1 GPIO5_PIN
-#define PIN_D2 GPIO4_PIN
-#define PIN_D3 GPIO0_PIN  // working as OUTPUT
-#define PIN_D4 GPIO2_PIN  // working as OUTPUT, ESP8266-12 built-in !LED
-#define PIN_D5 GPIO14_PIN // working as OUTPUT
-#define PIN_D6 GPIO12_PIN
-#define PIN_D7 GPIO13_PIN  // RXD2
-#define PIN_D8 GPIO15_PIN  // TXD2, working as OUTPUT
-#define PIN_D9 GPIO3_PIN   // RXDO,  if used will break serial communication (uC <- PC), working as OUTPUT
-#define PIN_D10 GPIO1_PIN  // TXDO, if used will break serial communication (uC -> PC), can work as OUTPUT
-#define PIN_D11 GPIO9_PIN  // SDD2 / SD2 // cannot be used at all, internal
-#define PIN_D12 GPIO10_PIN // SDD3 / SD3 // can be used as input only
 
 #endif // ESP8266
