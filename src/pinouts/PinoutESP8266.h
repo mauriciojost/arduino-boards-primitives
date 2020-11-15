89e3d5f9dfed05420549960892509ea522aebe23
Mauricio Jost <mauriciojost@gmail.com>
2020-11-15 07:10:11 +0100

    Update

--- src/pinouts/PinoutESP8266.h
+++ src/pinouts/PinoutESP8266.h
@@ -1,47 +1,37 @@
 #ifdef ESP8266 // NODEMCU based on ESP8266
 
-// CONSIDERATIONS FOR THE CIRCUIT 
+#define GPIO0_PIN 0 // *
+#define GPIO1_PIN 1 // 
+#define GPIO2_PIN 2 // *
+#define GPIO3_PIN 3 // 
+#define GPIO4_PIN 4 // 
+#define GPIO5_PIN 5 // 
+//#define GPIO6_PIN 6 // can't be used as connected to flash
+//#define GPIO7_PIN 7 // can't be used as connected to flash
+//#define GPIO8_PIN 8 // can't be used as connected to flash
+//#define GPIO9_PIN 9 // can't be used as connected to flash
+//#define GPIO10_PIN 10 // can't be used as connected to flash
+//#define GPIO11_PIN 11 // can't be used as connected to flash
+#define GPIO12_PIN 12 // 
+#define GPIO13_PIN 13 // 
+#define GPIO14_PIN 14 // 
+#define GPIO15_PIN 15 // *
+#define GPIO16_PIN 16 // 
+#define A0_PIN 17  // 
 
+// BOOT TRANSIENTS
+// The ESP8266 is a complicated non-trivial uC when it comes to behavior of its GPIO at boot. Some pins are not usable, some are output for a few tens of ms (even before getting to the setup() routine call), etc. Better read:
+https://rabbithole.wwwdotorg.org/2017/03/28/esp8266-gpio.html
+
+
+// BOOT MODE
+// (*) such pin participates in boot mode, so pull ups/down resistors are needed.
 // From 
 // - https://tttapa.github.io/ESP8266/Chap04%20-%20Microcontroller.html
 // - https://i.pinimg.com/originals/f7/81/ef/f781efedf44a3b9673f1e88d2d8253c8.png
 
-// GPIO15  GPIO0   GPIO2
-//     0       0       1   UART BOOTLOADER
-//     0       1       1   RUN MODE (in every boot, heads up or may unflash/corrupt your firmware)
-//
-// Heads up when having pull-up/pull-down resistors in such pins!
-//
-// ....
-// [testing if needed all below] use resistors of 500Kohm for such
-// Ensure:
-//   VDD-----220uF---GND
-//   EN------500kohm---VDD
-//   RST--+--500kohm---VDD
-//        \--470pf-----GND
-//   GPIO0---500kohm---VDD
-//   GPIO2---500kohm---VDD
-//   GPIO15--500kohm---VDD
-//   GPIO16------------RST (only in case of deep sleep) 
-
-
-#define GPIO0_PIN 0 // pull-up?, participates in boot mode
-#define GPIO1_PIN 1 // pull-up?
-#define GPIO2_PIN 2 // pull-up?, participates in boot mode
-#define GPIO3_PIN 3 // pull-up?
-#define GPIO4_PIN 4 // pull-up
-#define GPIO5_PIN 5 // pull-up
-//#define GPIO6_PIN 6 // pull-up, can't be used as connected to flash
-//#define GPIO7_PIN 7 // pull-up, can't be used as connected to flash
-//#define GPIO8_PIN 8 // pull-up, can't be used as connected to flash
-//#define GPIO9_PIN 9 // pull-up, can't be used as connected to flash
-//#define GPIO10_PIN 10 // pull-up, can't be used as connected to flash
-//#define GPIO11_PIN 11 // pull-up, can't be used as connected to flash
-#define GPIO12_PIN 12 // pull-up
-#define GPIO13_PIN 13 // pull-up
-#define GPIO14_PIN 14 // pull-up
-#define GPIO15_PIN 15 // pull-up, participates in boot mode
-#define GPIO16_PIN 16 // pull-down
-#define A0_PIN 17
+// GPIO0   GPIO2   GPIO15
+//     0       1       0    UART BOOTLOADER
+//    1       1        0    RUN MODE (in every boot, heads up or may unflash/corrupt your firmware)
 
 #endif // ESP8266
